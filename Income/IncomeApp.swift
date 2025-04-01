635ce0f4677a37730a9b1c68a7d612e5cfb1762c
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-04-01 11:37:24 -0300

    Initial Commit

---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Douglas de Mattos on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
