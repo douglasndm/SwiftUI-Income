68f26d0a7d0458b29499c67a43820763d1648a93
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-04-02 10:10:10 -0300

    TransactionView SubView

--- Income/HomeView.swift
+++ Income/HomeView.swift
@@ -10,44 +10,19 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var transacations: [Transaction] = [
+        Transaction(title: "Apple", type: .expense, amount: 5.00, date: Date()),
         Transaction(title: "Apple", type: .expense, amount: 5.00, date: Date())
     ]
     
     var body: some View {
         VStack {
             List {
-                ForEach(transacations) { transacation in
-                    VStack {
-                        HStack {
-                            Spacer();
-                            Text(transacation.displayDate)
-                                .font(.system(size: 14));
-                            Spacer();
-                        }
-                        .padding(.vertical, 5)
-                        .background(Color.lightGrayShade.opacity(0.5))
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                        
-                        HStack {
-                            Image(systemName: transacation.type == .income ? "arrow.up.forward" : "arrow.down.forward")
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundStyle(transacation.type == .income ? .red : .blue);
-                            
-                            VStack (alignment: .leading, spacing: 5) {
-                                HStack {
-                                    Text(transacation.title)
-                                        .font(.system(size: 15, weight: .bold))
-                                    Spacer();
-                                    Text(String(transacation.displayAmount))
-                                        .font(.system(size: 15, weight: .bold))
-                                };
-                                Text("Completed")
-                                    .font(.system(size: 14))
-                            }
-                        }
-                    }
+                ForEach(transacations) { transaction in
+                    TransactionView(transaction: transaction);
                 }
+                .listRowSeparator(.hidden)
             }
+            .scrollContentBackground(.hidden)
         }
     }
 }
---
+++ Income/Views/TransactionView.swift
@@ -0,0 +1,48 @@
+//
+//  TransactionView.swift
+//  Income
+//
+//  Created by Douglas de Mattos on 02/04/25.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    let transaction: Transaction;
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer();
+                Text(transaction.displayDate)
+                    .font(.system(size: 14));
+                Spacer();
+            }
+            .padding(.vertical, 5)
+            .background(Color.lightGrayShade.opacity(0.5))
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            HStack {
+                Image(systemName: transaction.type == .income ? "arrow.up.forward" : "arrow.down.forward")
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundStyle(transaction.type == .income ? .red : .blue);
+                
+                VStack (alignment: .leading, spacing: 5) {
+                    HStack {
+                        Text(transaction.title)
+                            .font(.system(size: 15, weight: .bold))
+                        Spacer();
+                        Text(String(transaction.displayAmount))
+                            .font(.system(size: 15, weight: .bold))
+                    };
+                    Text("Completed")
+                        .font(.system(size: 14))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TransactionView(transaction: Transaction(title: "Apple", type: .expense, amount: 5.00, date: Date()))
+}
